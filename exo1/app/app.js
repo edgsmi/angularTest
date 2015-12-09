bc3fe8fd372372d7937fde023ab7aed97bf49009
esmith <edgsmi@gmail.com>
2015-12-09 15:27:54 +0100

    add country.json

--- exo1/app/app.js
+++ exo1/app/app.js
@@ -1,32 +1,6 @@
 (function() {
 	'use strict';
 	
-	var countryHash = {
-		'France': {
-			wikipedia: 'https://fr.wikipedia.org/wiki/France',
-			capital: 'Paris'
-		},
-		'Allemagne': {
-			wikipedia: 'https://fr.wikipedia.org/wiki/Allemagne',
-			capital: 'Berlin'
-		},
-		'Italie': {
-			wikipedia: 'https://fr.wikipedia.org/wiki/Italie',
-			capital: 'Rome'
-		},
-		'Espagne': {
-			wikipedia: 'https://fr.wikipedia.org/wiki/Espagne',
-			capital: 'Madrid'
-		},
-		'Suisse': {
-			wikipedia: 'https://fr.wikipedia.org/wiki/Suisse',
-			capital: 'Berne'
-		},
-		'USA': {
-			wikipedia: 'https://fr.wikipedia.org/wiki/Suisse',
-			capital: 'Washington DC'
-		}
-	};
 	
 	var countries2 = [
 		{ name: 'FRANCE', title: 'France'},
@@ -34,7 +8,7 @@
 	];
 	
 
-	var app = angular.module('myApp', ['ngRoute', 'angucomplete-alt']);
+	var app = angular.module('myApp', ['ngRoute', 'angucomplete-alt', 'ngResource']);
 
 	app.config(['$routeProvider', function($routeProvider) {
 
@@ -56,11 +30,44 @@
 			});
 	}]);
 
-	app.run(['$rootScope', function($rootScope) {
-		$rootScope.countries = Object.keys(countryHash);
-		$rootScope.countryHash = countryHash;
+
+/*	app.run(['$rootScope', '$resource', function($rootScope, $resource) {
+		//$rootScope.countryHash = countryHash;
+		$rootScope.countryHash = $resource('country.json').get();
+		$rootScope.$watch('countryHash', function() {
+			$rootScope.countries = Object.keys($rootScope.countryHash)
+			.filter(function(n) {
+				return !(n == '$promise' || n == '$resolved');
+			});
+		}, true);
 		$rootScope.countries2 = countries2;
 	}]);
+*/
+	app.run(['$rootScope', '$resource', function($rootScope, $resource) {
+		$rootScope.countryHash = '';
+		$resource('country.json').get()
+		.$promise.then(function(result) {
+     		$rootScope.countryHash = result['country'];
+   		 });
+		$rootScope.$watch('countryHash', function() {
+			$rootScope.countries = Object.keys($rootScope.countryHash);
+		}, true);
+		$rootScope.countries2 = countries2;
+	}]);
+
+/*	app.run(['$rootScope', '$resource', '$http', function($rootScope, $resource, $http) {
+		$http.get('country.json').then(function(response) {
+			$rootScope.countryHash = response.data;
+			$rootScope.$watch('countryHash', function() {
+				$rootScope.countries = Object.keys($rootScope.countryHash);
+			});
+		}).catch(function(error) {
+			$rootScope.countryHash = '';
+			$rootScope.countries = '';
+			console.log('Error', error);
+		});
+		$rootScope.countries2 = countries2;
+	}]);*/
 
 	 app.controller('MainController', function($scope, $route, $routeParams, $location) {
 	     $scope.$route = $route;
